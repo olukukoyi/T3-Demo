050b9849e1ec926361a8b1e20c5e57e9285ac9aa
olukukoyi <oluwanifesimi.kukoyi15@gmail.com>
2023-02-26 13:03:07 -0500

    fixed signin function

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,6 +1,6 @@
-import { GetServerSideProps, type NextPage } from "next";
-import { getProviders, signIn, signOut, useSession } from "next-auth/react";
-import { type AppProps } from "next/app";
+import { type GetServerSideProps } from "next";
+import { getProviders, signIn } from "next-auth/react";
+// import { type AppProps } from "next/app";
 
 type Provider = {
   id: string;
@@ -20,8 +20,22 @@ const Signin = ({ providers }: SigninProps) => {
       <h1>Signin</h1>
       <div>
         {Object.values(providers).map((provider) => (
-          <button key={provider.id}>Sign in with Google</button>
+          <button
+            key={provider.id}
+            onClick={() =>
+              void signIn(provider.id, {
+                callbackUrl: `${window.location.origin}`,
+              })
+            }
+          >
+            Sign in with Google
+          </button>
         ))}
+        {/* void (async () => {
+                await signIn(provider.id, {
+                  callBackUrl: `${window.location.origin}/}`,
+                });
+              }) */}
       </div>
     </>
   );
